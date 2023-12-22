73960eaab377b8b1936e8df8139f91b7c478e133
Chris Awoke <uchechristian27@gmail.com>
2023-12-22 14:30:24 +0100

    Update Logo.jsx

--- components/Header/Logo.jsx
+++ components/Header/Logo.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark">
-      <div className="w-10 rounded-full overflow-hidden border border-solid border-dark">
+      <div className="w-10 rounded-full overflow-hidden border border-solid border-dark mr-2">
         <Image
           src="/assets/profile-img.png"
           alt="Logo"
@@ -13,6 +13,7 @@ const Logo = () => {
           className="w-full h-auto rounded-full"
         />
       </div>
+      <span className="font-bold text-xl">Chris Awoke</span>
     </Link>
   );
 };
