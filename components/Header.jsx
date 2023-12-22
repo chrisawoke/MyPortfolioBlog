b0c94697cd08746248ab8e522eeafa87977fc05c
Chris Awoke <uchechristian27@gmail.com>
2023-12-22 14:03:34 +0100

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="">Index</header>;
+  return (
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+      <h1>Index</h1>
+    </header>
+  );
 };
 
 export default Header;
