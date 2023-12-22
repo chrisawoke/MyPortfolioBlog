2feae0b2c9cf2112837bafc33bb740b948d2ab33
Chris Awoke <uchechristian27@gmail.com>
2023-12-22 13:36:20 +0100

    latest update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,17 @@
-import { Inter } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-in",
+});
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mr",
+});
 
 export const metadata = {
   title: "Chris Awoke",
@@ -11,7 +21,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cx(
+          inter.variable,
+          manrope.variable,
+          "font-mr bg-light dark:bg-dark"
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
